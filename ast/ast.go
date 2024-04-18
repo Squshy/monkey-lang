6186392bee482e0a94f73eb2f8fb1605b64a8044
Calvin Lapp <lapp.cd@gmail.com>
2024-04-18 19:38:58 -0400

    ast

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// The root node of every AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier // name of the binding
+	Value Expression  // expression which produces the value
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
