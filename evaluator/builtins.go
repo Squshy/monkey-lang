021eb9dc5703b7d935b4ad49b998ebdaf1114523
Calvin Lapp <lapp.cd@gmail.com>
2024-11-06 20:54:10 -0500

    Added puts printable fn

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{Fn: func(args ...object.Object) object.Object {
@@ -87,5 +90,12 @@ var builtins = map[string]*object.Builtin{
 		newElements[length] = args[1]
 
 		return &object.Array{Elements: newElements}
+	}},
+	"puts": &object.Builtin{Fn: func(args ...object.Object) object.Object {
+		for _, arg := range args {
+			fmt.Println(arg.Inspect())
+		}
+
+		return NULL
 	}},
 }
